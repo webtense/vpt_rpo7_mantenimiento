9a081659ad94e13107bf8b2435ac6ad7542705f7
root <root@DebOdoo16.boitaull.com>
2024-12-13 15:11:03 +0000

    Actualización de versión a 16.0.7

--- __manifest__.py
+++ __manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'VPT RPO7 Mantenimiento',
-    'version': '16.0.6',
+    'version': '16.0.7',
     'category': 'Maintenance',
     'summary': 'Gestión integral de mantenimiento para hoteles',
     'description': """
