b38527d7f4ce0e4338f10697c138db4858373fea
root <root@DebOdoo16.boitaull.com>
2024-12-15 19:11:33 +0000

    Actualización de versión a 16.1.1

--- __manifest__.py
+++ __manifest__.py
@@ -1,22 +1,26 @@
 # -*- coding: utf-8 -*-
 {
-    'name': 'VPT RPO7 Mantenimiento',
-    'version': '16.0.12',
+    'name': 'RPO 7.0 Mantenimiento',
+    'version': '16.1.1',
     'category': 'Maintenance',
     'summary': 'Gestión integral de mantenimiento para hoteles',
     'description': """
         Este módulo proporciona una gestión integral del mantenimiento,
-        cubriendo desde inspecciones obligatorias hasta tareas diarias,
-        con un enfoque en la planificación preventiva y el seguimiento
-        detallado de todas las actividades de mantenimiento en los hoteles.
+        cubriendo desde inspecciones obligatorias hasta tareas diarias.
+        Funciones principales:
+        - Creación y asignación de tareas de mantenimiento.
+        - Registro de inspecciones obligatorias y preventivas.
+        - Control de repuestos y proveedores autorizados.
+        - Configuración de planes de mantenimiento por instalación.
     """,
     'author': 'Andrés Sánchez',
     'website': 'https://www.boitaullresort.com',
     'depends': ['base', 'mail'],
     'data': [
-        'security/ir.model.access.csv',
-#        'views/action_dashboard_views.xml',
+#        'security/ir.model.access.csv',
+        'views/action_dashboard_views.xml',
         'views/menu_views.xml',
+        'views/libro_direccion_views.xml',
         'views/inspecciones_obligatorias_views.xml',
         'views/plan_mantenimiento_views.xml',
         'views/mantenimiento_preventivo_views.xml',
@@ -29,9 +33,8 @@
         'views/control_detectores_views.xml',
         'views/inspecciones_extintores_views.xml',
         'views/tareas_exteriores_views.xml',
-#        'views/libro_direccion_views.xml',
     ],
-    'demo': [],
+    'images': ['static/description/inspecciones.png'],
     'installable': True,
     'application': True,
     'auto_install': False,
